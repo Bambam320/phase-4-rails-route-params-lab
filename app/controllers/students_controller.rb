4fa01b6acb9bd3ba7d2ffebe9228a6f9cddfa533
Bambam320 <igor.mestechkin@gmail.com>
2022-11-09 01:35:05 -0500

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name] 
+      students = Student.where(last_name: params[:name].capitalize).or(Student.where(first_name: params[:name].capitalize))
+      render json: students
+    else
+      students = Student.all
+      render json: students
+    end
+  end
+
+  def show
+    found = Student.find(params[:id])
+    render json: found
   end
 
 end
